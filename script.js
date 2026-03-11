571dbe1e95b3685fc7b717a2e63fe1b53fa4d8a6
benbeyert-cloud <BenBeyert@gmail.com>
2026-03-11 13:23:27 +0100

    pushing my website

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('about-btn').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('process-btn').addEventListener('click', function() {
+    document.getElementById('process').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('projects-btn').addEventListener('click', function() {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
